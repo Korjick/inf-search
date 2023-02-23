b1ccfb47d3d3fa74e938429d1b356f5e4ec8af87
Bulat Din <zzgriim@gmail.com>
2023-02-24 02:31:16 +0300

    Crawler

--- main.py
+++ main.py
@@ -2,44 +2,34 @@ from __future__ import print_function
 
 from lxml import html
 import requests
-
-
-def get_a_nofollow_lxml(url="https://sites.google.com/site/top1000gou/"):
-    r = requests.get(url)
-    html_source = r.text
-    root_element = html.fromstring(html_source)
-    return root_element.find_rel_links("nofollow")
-
-
-def try_download_and_save_pages(urls, limit=100):
-    with open('./index.txt', 'w') as file:
-        i = 1
-        final_output = ''
-        for idx, url in enumerate(urls):
-            if try_download_and_save_page(url, i):
-                final_output += str(i) + ' - ' + url + '\n'
-                i += 1
-            if i > limit:
-                break
-        file.write(final_output)
-
-
-def try_download_and_save_page(url, i):
-    headers = {"Accept-Language": "en-US,en;q=0.5"}
-    try:
-        r = requests.get(url, headers=headers)
-        if len(html.fromstring(r.text).text_content()) < 1000:
-            return False
-        with open('./' + str(i) + '.html', 'w') as file:
-            file.write(r.text)
-        return True
-    except:
-        return False
+import csv
 
 
 if __name__ == '__main__':
-    a_nofollow = get_a_nofollow_lxml()
-    urls = []
-    for element in a_nofollow:
-        urls.append(element.get('href'))
-    try_download_and_save_pages(urls, limit=5)
+    headers = {"Accept-Language": "en-US,en;q=0.5"}
+    with open("./builtwith-top1m-20230221.csv", 'r') as file:
+        with open('./index.txt', 'w') as index:
+            csvreader = csv.reader(file)
+            i = 1
+            final_output = ''
+            for row in csvreader:
+                try:
+                    if i > 100:
+                        break
+                    url = 'https://' + row[1]
+                    r = requests.get(url, headers=headers, timeout=5)
+                    r.encoding = 'utf-8'
+
+                    if r.status_code != 200:
+                        continue
+
+                    if len(html.fromstring(r.text).text_content()) < 1000:
+                        continue
+
+                    with open('./' + str(i) + '.html', 'w') as htmtext:
+                        htmtext.write(r.text)
+                        final_output += str(i) + ' - ' + url + '\n'
+                        i += 1
+                except:
+                    continue
+            index.write(final_output)
